9c36cf2feca0ad28168c2182fb3082d2440f2b4d
Brian Mak <eggrollbot@gmail.com>
2021-08-26 14:21:14 -0700

    Added events to the ChetoRpEvents class

--- code/Base/Event/ChetoRpEvents.cs
+++ code/Base/Event/ChetoRpEvents.cs
@@ -5,6 +5,37 @@
 	/// </summary>
 	public static class ChetoRpEvents
 	{
+		// Game events.
+
+		/// <summary>
+		/// The name of the event run before the game initializes.
+		/// </summary>
+		public const string PreGameInit = "PreGameInit";
+
+		public const string PostGameInit = "PostGameInit";
+
+		public const string OnGameShutdown = "OnGameShutdown";
+
+		public const string PreModuleInit = "PreModuleInit";
+
+		public const string PostModuleInit = "PostModuleInit";
+
+		public const string OnAllModulesInit = "OnAllModulesInit";
+
+		// Player events.
+
+		public const string PrePlayerSpawn = "PrePlayerSpawn";
+
+		public const string PostPlayerSpawn = "PostPlayerSpawn";
+
+		public const string PrePlayerTakeDamage = "PrePlayerTakeDamage";
+
+		public const string PostPlayerTakeDamage = "PostPlayerTakeDamage";
+
+		public const string PrePlayerDeath = "PrePlayerDeath";
+
+		public const string PostPlayerDeath = "PostPlayerDeath";
+
 		/// <summary>
 		/// The name of the event run prior to a config change.
 		/// </summary>

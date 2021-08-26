868cf06a7262f7898fd5514551e2861cdb643728
Brian Mak <eggrollbot@gmail.com>
2021-08-26 15:59:36 -0700

    Created a module for testing

---
+++ code/Team/TeamModule.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+
+namespace ChetoRp.Employment
+{
+	/// <summary>
+	/// The config store class for <see cref="TeamModule"/>.
+	/// </summary>
+	public class TeamModuleConfig
+	{
+	}
+
+	/// <summary>
+	/// The <see cref="TeamModule"/> entrypoint class.
+	/// </summary>
+	[ChetoRpModule]
+	public class TeamModule : Module<TeamModuleConfig>
+	{
+		/// <summary>
+		/// The entrypoint to the module.
+		/// </summary>
+		protected internal override void Run()
+		{
+			Log.Info( "Initialized the team module." );
+		}
+	}
+}

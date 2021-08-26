95d888a08e81b4fa1f184f27c8530bc1024a5808
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2021-08-26 13:05:51 -0700

    Added ChetoRP constant event names into a static class

---
+++ code/Base/Event/ChetoRpEvents.cs
@@ -0,0 +1,18 @@
+﻿namespace ChetoRp
+{
+	/// <summary>
+	/// A static class containing constant event names.
+	/// </summary>
+	public static class ChetoRpEvents
+	{
+		/// <summary>
+		/// The name of the event run prior to a config change.
+		/// </summary>
+		public const string OnPreConfigChange = "PreConfigChange";
+
+		/// <summary>
+		/// The name of the event run after a config change.
+		/// </summary>
+		public const string OnPostConfigChange = "PostConfigChange";
+	}
+}

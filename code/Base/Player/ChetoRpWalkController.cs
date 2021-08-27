42323cb2a0f23a0f633234f11461a91036fc6a6e
Brian Mak <eggrollbot@gmail.com>
2021-08-27 14:17:13 -0700

    Implemented fall damage

--- code/Base/Player/ChetoRpWalkController.cs
+++ code/Base/Player/ChetoRpWalkController.cs
@@ -7,5 +7,37 @@ namespace ChetoRp
 	/// </summary>
 	public class ChetoRpWalkController : WalkController
 	{
+		/// <summary>
+		/// The minimum speed the player needs to be going down vertically to take damage.
+		/// </summary>
+		public float MinFallDamageSpeed { get; set; } = 500;
+
+		/// <summary>
+		/// The damping of the fall damage value. The formula for fall damage is FallSpeed / FallDamageDamping.
+		/// </summary>
+		public int FallDamageDamping { get; set; } = 20;
+
+		/// <summary>
+		/// Simulates the player's movements.
+		/// </summary>
+		public override void Simulate()
+		{
+			bool wasOnGround = GroundEntity != null;
+			float fallSpeed = Velocity.z >= 0 ? 0 : -Velocity.z;
+			base.Simulate();
+			bool isOnGround = GroundEntity != null;
+
+			if ( !wasOnGround && isOnGround && fallSpeed >= MinFallDamageSpeed )
+			{
+				DamageInfo damageInfo = new()
+				{
+					Attacker = Entity.FindByIndex( 0 ), // World.
+					Damage = ( fallSpeed - MinFallDamageSpeed ) / FallDamageDamping,
+					Flags = DamageFlags.Fall
+				};
+
+				Pawn.TakeDamage( damageInfo );
+			}
+		}
 	}
 }

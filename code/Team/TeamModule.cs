a0af076c83b45a9101b6d5831509803bb05fbd37
Brian Mak <eggrollbot@gmail.com>
2021-08-27 16:10:48 -0700

    Added attributes to configs for modules

--- code/Team/TeamModule.cs
+++ code/Team/TeamModule.cs
@@ -5,8 +5,14 @@ namespace ChetoRp.Employment
 	/// <summary>
 	/// The config store class for <see cref="TeamModule"/>.
 	/// </summary>
+	[ChetoRpConfigObject]
 	public class TeamModuleConfig
 	{
+		/// <summary>
+		/// The teams in the game.
+		/// </summary>
+		[ChetoRpConfigOptionInfo( "The teams in the game." )]
+		public Team[] Teams { get; set; } = new Team[] { new Team( "Test" ) };
 	}
 
 	/// <summary>

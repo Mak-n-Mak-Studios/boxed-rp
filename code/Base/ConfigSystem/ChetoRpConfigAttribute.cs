e34b58c144e77d98d8ecfb155b0c9599c286411f
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2021-08-26 17:21:47 -0700

    Added config and config option attributes

---
+++ code/Base/ConfigSystem/ChetoRpConfigAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+using System;
+
+namespace ChetoRp
+{
+	/// <summary>
+	/// An attribute to apply to all config stores.
+	/// </summary>
+	[AttributeUsage( AttributeTargets.Class )]
+	public class ChetoRpConfigAttribute : LibraryAttribute { }
+}
---
+++ code/Base/ConfigSystem/ChetoRpConfigOptionDescriptionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChetoRp
+{
+	/// <summary>
+	/// The attribute to put on all config options properties within config stores
+	/// </summary>
+	[AttributeUsage( AttributeTargets.Property )]
+	class ChetoRpConfigOptionDescriptionAttribute : Attribute
+	{
+		/// <summary>
+		/// The description of the config option that this attribute is applied to.
+		/// </summary>
+		public string Description { get; }
+
+		/// <summary>
+		/// Initializes a new<see cref="ChetoRpConfigOptionDescriptionAttribute"/>.
+		/// </summary>
+		/// <param name="description">The config option description.</param>
+		public ChetoRpConfigOptionDescriptionAttribute( string description )
+		{
+			Description = description;
+		}
+	}
+}

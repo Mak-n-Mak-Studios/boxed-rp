f5e93d7639cbf2a95b8f130c6edb25fb31f73273
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2021-08-27 22:17:13 -0700

    Added a clarification to the [ChetoRpConfigObject] attribute documentation header

--- code/Base/ConfigSystem/ChetoRpConfigObjectAttribute.cs
+++ code/Base/ConfigSystem/ChetoRpConfigObjectAttribute.cs
@@ -8,6 +8,9 @@ namespace ChetoRp
 	/// An attribute to apply to all config stores and all objects that can be used 
 	/// within them with serializable properties. Internally, this is used to read
 	/// properties with [<see cref="ChetoRpConfigOptionInfoAttribute"/>] on them.
+	/// <br/><br/><b>IMPORTANT</b>: Classes marked with this attribute must have
+	/// a parameterless constructor because they will be instantiated
+	/// every time the config file is rewritten.
 	/// </summary>
 	[AttributeUsage( AttributeTargets.Class )]
 	public class ChetoRpConfigObjectAttribute : LibraryAttribute { }

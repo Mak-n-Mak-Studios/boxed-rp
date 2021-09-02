88fdb88e2c8c044c6450855387dd5ca6d28b7caf
Brian Mak <eggrollbot@gmail.com>
2021-09-02 14:48:31 -0700

    Clarified naming convention for localization properties

--- code/Base/ConfigSystem/ConfigLocalizedPropertyAttribute.cs
+++ code/Base/ConfigSystem/ConfigLocalizedPropertyAttribute.cs
@@ -7,7 +7,7 @@ namespace ChetoRp
 	/// <summary>
 	/// The attribute to put on properties within <see cref="ILocale"/> when
 	/// they're localized config option descriptions. Name of the property should
-	/// be "[ClassName][OptionName]".
+	/// be "[ClassName][OptionName]". Class name should not include "Module".
 	/// </summary>
 	[AttributeUsage( AttributeTargets.Property, AllowMultiple = false )]
 	public class ConfigLocalizedPropertyAttribute : LocalizationDataPropertyAttribute { }

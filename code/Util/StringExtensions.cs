5d8623c49f6824bf1ec68c56ede86e2f9c533972
Brian Mak <eggrollbot@gmail.com>
2021-09-02 18:12:39 -0700

    Added an extension method for string.Format

---
+++ code/Util/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace ChetoRp
+{
+	/// <summary>
+	/// A static class containing extension methods for <see cref="string"/>s.
+	/// </summary>
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// A convenience instance method for string.Format.
+		/// </summary>
+		/// <param name="str">The format string.</param>
+		/// <param name="arg">The object to format.</param>
+		/// <returns>The formatted string.</returns>
+		public static string Format( this string str, object arg )
+		{
+			return string.Format( str, arg );
+		}
+
+		/// <summary>
+		/// A convenience instance method for string.Format.
+		/// </summary>
+		/// <param name="str">The format string.</param>
+		/// <param name="arg">The objects to format.</param>
+		/// <returns>The formatted string.</returns>
+		public static string Format( this string str, params object[] arg )
+		{
+			return string.Format( str, arg );
+		}
+	}
+}

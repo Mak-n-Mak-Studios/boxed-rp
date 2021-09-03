29f58b4db595f0f3c54faf8d723b1e8520969d06
Brian Mak <eggrollbot@gmail.com>
2021-09-02 20:08:14 -0700

    Translated text to Uruguayan Spanish

--- code/Localization/Locales/UruguayanSpanish.cs
+++ code/Localization/Locales/UruguayanSpanish.cs
@@ -20,47 +20,47 @@ namespace ChetoRp.Localization.Locales
 
 		public string BaseFinishedLoading => "Se cargó ChetoRP.";
 		public string BaseClientJoined => "\"{0}\" se unió a la partida.";
-		public string BaseFailedToLoadModule => "Module of type, {0}, failed to load."; // TO-DO: Translate.
-		public string BaseTrueFalse => "true/false"; // TO-DO: Translate.
-		public string BaseInteger => "integer"; // TO-DO: Translate.
-		public string BaseIntegerDecimal => "integer/decimal"; // TO-DO: Translate.
-		public string BaseCharacter => "character"; // TO-DO: Translate.
-		public string BaseText => "text"; // TO-DO: Translate.
-		public string BaseDescription => "Description"; // TO-DO: Translate.
-		public string BaseDefaultValueOfProperty => "Default value of {0}"; // TO-DO: Translate.
-		public string BaseLayoutOfType => "Layout of {0}"; // TO-DO: Translate.
-		public string BaseTypeContainsNothing => "{0} contains nothing."; // TO-DO: Translate.
-		public string BaseBeginningOfTypeDocumentation => "BEGINNING OF {0} DOCUMENTATION"; // TO-DO: Translate.
-		public string BaseEndOfTypeDocumentation => "END OF {0} DOCUMENTATION"; // TO-DO: Translate.
-		public string BaseConfigMinFallDamageSpeed => "The minimum speed the player needs to be going down vertically to take damage."; // TO-DO: Translate.
-		public string BaseConfigFallDamageDamping => "The damping of the fall damage value. The formula for fall damage is FallSpeed / FallDamageDamping."; // TO-DO: Translate.
+		public string BaseFailedToLoadModule => "No se pudo cargar el módulo del tipo, {0}.";
+		public string BaseTrueFalse => "true/false (sí/no)";
+		public string BaseInteger => "integral";
+		public string BaseIntegerDecimal => "integral/decimal";
+		public string BaseCharacter => "carácter";
+		public string BaseText => "texto";
+		public string BaseDescription => "Descripción";
+		public string BaseDefaultValueOfProperty => "El valor por defecto de {0}";
+		public string BaseLayoutOfType => "Estructura de {0}";
+		public string BaseTypeContainsNothing => "{0} no tiene nada.";
+		public string BaseBeginningOfTypeDocumentation => "COMIENZO DE LA DOCUMENTACIÓN DE {0}";
+		public string BaseEndOfTypeDocumentation => "FIN DE LA DOCUMENTACIÓN DE {0}";
+		public string BaseConfigMinFallDamageSpeed => "La velocidad mínima para abajo que tiene que ir el jugador para dañarse.";
+		public string BaseConfigFallDamageDamping => "Cuánto se va a reducir el daño de caída. La fórmula para daño de caída es Velocidad / FallDamageDamping (reducción de daño de caída).";
 
 		#endregion
 
 		#region Employment
 
-		public string EmploymentWeaponDoesNotExist => "The provided team has a weapon that does not exist: \"{0}\"."; // TO-DO: Translate.
-		public string EmploymentConfigSalaryPayPeriod => "The number of seconds between each salary pay period."; // TO-DO: Translate.
-		public string EmploymentConfigDefaultWeapons => "The default weapons given to all players."; // TO-DO: Translate.
-		public string EmploymentConfigTeams => "The teams in the game."; // TO-DO: Translate.
+		public string EmploymentWeaponDoesNotExist => "El equipo, {0}, tiene un arma que no existe: \"{1}\".";
+		public string EmploymentConfigSalaryPayPeriod => "El tiempo en segundos entre cada período de pago.";
+		public string EmploymentConfigDefaultWeapons => "Las armas por defecto recibidos por todos los jugadores.";
+		public string EmploymentConfigTeams => "Los equipos en el juego.";
 		public string TeamConfigCategory => "La categoría del equipo.";
 		public string TeamConfigPrettyName => "El nombre para mostrar del equipo.";
 		public string TeamConfigDescription => "La descripción del equipo.";
 		public string TeamConfigModels => "Los modelos posibles para el equipo.";
 		public string TeamConfigWeapons => "Las armas que se le dan a los del equipo cuando spawnean.";
 		public string TeamConfigShouldGetDefaultWeapons => "Si se le deberían dar las armas por defecto al equipo.";
-		public string TeamConfigMaxPlayers => "La cantidad máxima de jugadores que pueden ser de este equipo a la vez. 0 = sin límite. Will be ignored if the team is the default team."; // TO-DO: Translate.
+		public string TeamConfigMaxPlayers => "La cantidad máxima de jugadores que pueden ser de este equipo a la vez. 0 = sin límite. Se va a ignorar esta configuración si este es el equipo por defecto.";
 		public string TeamConfigSalary => "El sueldo que se le da a los jugadores después de cada período de pago.";
 		public string TeamConfigIsVoteRequired => "Si se necesita votar para entrar al equipo.";
 		public string TeamConfigCanBeDemoted => "Si se pueden rebajar los jugadores del equipo.";
 		public string TeamConfigIsDefault => "Si el equipo por defecto debería ser este cuando se une un jugador. El primer equipo encontrado con esta configuración activada en el archivo de configuración va a ser el equipo por defecto.";
-		public string TeamConfigTeamType => "Si el equipo es especial.";
+		public string TeamConfigTeamType => "El tipo del equipo.";
 
 		#endregion
 
 		#region Localization
 
-		public string LocalizationConfigLocale => "The locale to use. By changing this option on the server, configs are regenerated into this locale but will not affect the clients' locales. The client can use the /changelanguage command."; // TO-DO: Translate.
+		public string LocalizationConfigLocale => "El idioma y región para usar. Si cambiás esta opción en las configuraciones del servidor, se van a regenerar todos los archivos de configuración para que usen este idioma pero no va a afectar a los clientes. Para cambiar su idioma y región, los clientes pueden usar el comando /changelanguage.";
 
 		#endregion
 	}
